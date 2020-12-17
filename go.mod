29913989fc590e120da3dedecddbe1ca630f4244
yuki-eto <yuki.eto@toydium.co.jp>
2020-12-17 23:16:22 +0900

    using yyoshiki41/go-radiko

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module radikolive
 
 go 1.15
 
-require github.com/yuki-eto/go-radiko v0.7.0 // indirect
+require (
+	github.com/grafov/m3u8 v0.11.1 // indirect
+	github.com/yyoshiki41/go-radiko v0.7.0
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
+)
--- main.go
+++ main.go
@@ -1,32 +1,42 @@
 package main
 
 import (
-	"bufio"
 	"context"
+	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os/exec"
-	"strings"
+	"time"
 
-	"github.com/yuki-eto/go-radiko"
+	"github.com/yyoshiki41/go-radiko"
 )
 
 func main() {
 	var (
-		user string
-		pass string
-		station string
+		user   string
+		pass   string
+		isList bool
 	)
 	flag.StringVar(&user, "user", "", "username")
 	flag.StringVar(&pass, "pass", "", "password")
-	flag.StringVar(&station, "station", "LFR", "station code")
+	flag.BoolVar(&isList, "list", false, "show station list")
 	flag.Parse()
 
 	client, err := radiko.New("")
 	if err != nil {
 		log.Fatalf("%+v", err)
+	}
+
+	if isList {
+		infos, err := getStationPrograms(client)
+		if err != nil {
+			log.Fatalf("%+v", err)
+		}
+		for _, s := range infos {
+			log.Print(s)
+		}
+		return
 	}
 
 	if user != "" && pass != "" {
@@ -45,6 +55,33 @@ func main() {
 		log.Fatalf("%+v", err)
 	}
 
+	station := flag.Arg(0)
+	if station == "" {
+		log.Print("please select station ID")
+		return
+	}
+
+	info, err := getStationProgram(client, station)
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	log.Print(info)
+
+	go func() {
+		ticker := time.NewTicker(time.Minute)
+		for range ticker.C {
+			newInfo, err := getStationProgram(client, station)
+			if err != nil {
+				log.Printf("%+v", err)
+				continue
+			}
+			if info != newInfo {
+				log.Print(newInfo)
+				info = newInfo
+			}
+		}
+	}()
+
 	mplayer := exec.Command(
 		"mplayer",
 		"-cache", "32",
@@ -59,32 +96,49 @@ func main() {
 	)
 	log.Print(cmd.String())
 
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatalf("%+v", err)
-	}
-	defer stdout.Close()
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.Fatalf("%+v", err)
-	}
-	defer stderr.Close()
-
-	readerFunc := func(reader io.Reader) {
-		bufReader := bufio.NewReader(reader)
-		for {
-			msg, err := bufReader.ReadString('\n')
-			if err != nil {
-				return
-			}
-			msg = strings.TrimSpace(msg)
-			fmt.Println(msg)
-		}
-	}
-	go readerFunc(stdout)
-	go readerFunc(stderr)
-
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("%+v", err)
 	}
+}
+
+func getStationProgram(client *radiko.Client, id string) (string, error) {
+	stations, err := client.GetNowPrograms(context.Background())
+	if err != nil {
+		return "", err
+	}
+	for _, s := range stations {
+		if id != s.ID {
+			continue
+		}
+		return getStationString(s), nil
+	}
+	return "", errors.New("cannot find station")
+}
+
+func getStationPrograms(client *radiko.Client) ([]string, error) {
+	stations, err := client.GetNowPrograms(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	var infos []string
+	for _, s := range stations {
+		infos = append(infos, getStationString(s))
+	}
+	return infos, nil
+}
+
+func getStationString(s radiko.Station) string {
+	prog := s.Scd.Progs.Progs[0]
+	const parseFormat = "20060102150405"
+	const displayFormat = "15:04:05"
+	from, _ := time.Parse(parseFormat, prog.Ft)
+	to, _ := time.Parse(parseFormat, prog.To)
+	return fmt.Sprintf(
+		"%s: %s / %s [%s - %s]",
+		s.ID,
+		s.Name,
+		prog.Title,
+		from.Format(displayFormat),
+		to.Format(displayFormat),
+	)
 }
